ff543139122db09454f394b233d803c65f59ed49
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2020-04-30 18:53:12 -0600

    Update Calculator.s

--- Calculator.s
+++ Calculator.s
@@ -35,8 +35,10 @@ main:
 	and r4,#0
 	and r5,#0
 	and r6,#0
+	b menu_loop
 	
 menu_loop:
+
 	ldr r0,=menu_calc	/* Mostrar menu principal */
 	bl puts
 	ldr r0,=opcion_msg	/* Mostrar mensaje opcion_msg */
@@ -44,6 +46,34 @@ menu_loop:
 	bl getchar			/* Obtener char eleccion */
 	ldr r1,=opcion
 	strb r0,[r1]
+	
+	ldrb r5,[r1]
+	
+	b check
+	
+check:
+	cmp r5, #'+' 
+	beq subroutine_suma	/* Si r5 = + suma */
+	
+	cmp r5, #'-'
+	beq subroutine_resta	/* Si r5 = - resta */
+
+	cmp r5, #'*'
+	beq subroutine_multip	/* Si r5 = * mutiplicacion */
+
+	cmp r5, #'M'
+	beq subroutine_div	/* Si r5 = M division */
+
+	cmp r5, #'P'
+	beq subroutine_pot	/* Si r5 = P potencia */
+
+	cmp r5, #'='
+	beq subroutine_tot	/* Si r5 = = resultado */
+
+	cmp r5, #'q'
+	beq _exit		/* Si r5 = q salida */
+
+	bne invalido 	@@En caso de no ser ningun valor irse a la funcion
 
 _exit:
 	mov	r3, #0	
