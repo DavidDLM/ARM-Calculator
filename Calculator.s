48ab56925b8634ecb1ae421d9627de032697d7ae
rom19199 <54779748+rom19199@users.noreply.github.com>
2020-05-05 14:30:40 -0600

    Update Calculator.s

--- Calculator.s
+++ Calculator.s
@@ -11,9 +11,9 @@
 op_1:	.asciz "El operando 1 es: %d \n"
 op_2:	.asciz "El operando 2 es: %d \n"
 
-res_sum: .asciz "El resultado de la suma es: %d \n"
-res_res: .asciz "El resultado de la resta es: %d \n"
-res_mul: .asciz "El resultado de la multiplicacion es: %d \n"
+res_sum: .asciz "El resultado  es: %d \n"
+res_res: .asciz "El resultado  es: %d \n"
+res_mul: .asciz "El resultado  es: %d \n"
 res_div: .asciz "El resultado de la division es: %d \n"
 res_div_res: .asciz "El residuo es: %d \n"
 res_pot: .asciz "El resultado de la potencia es: %d \n"
@@ -25,7 +25,6 @@ getop:	.asciz "%d"
 opcion: .byte ' '
 op: .word 0
 txt_operando: .asciz "Numero a operar: "
-error: .asciz "Numero no puede ser 0"
 
 /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 
@@ -37,68 +36,46 @@ error: .asciz "Numero no puede ser 0"
 main:
 	stmfd	sp!, {lr}	/* El resultado se ira almacenando en R5. R4 es el input del usuario. */
 	
-	and r4,#0
+	mov r4,#0
 	
 	@NUMERO INICIAL 0
-	and r5,#0
+	@and r5,#0
 	
 	@RESULTADO R6
-	and r6,#0
+	@and r6,#0
 	
-	b menu_loop
+	@b menu_loop
 	
-menu_loop:
+	
 	ldr r0,=menu_calc	/* Mostrar menu principal */
 	bl puts
-	ldr r0,=opcion_msg	/* Mostrar mensaje opcion_msg */
-	bl puts
-	bl getchar			/* Obtener char eleccion */
-	ldr r1,=opcion
+	bl getchar
+	
+	ldr r1,=opcion_msg	/* Mostrar mensaje opcion_msg */
 	strb r0,[r1]
 	
-	ldrb r4, [r1]	/* Cargar char a r4 */
+	ldr r4,= opcion_msg
+	ldrb r4,[r4]
+	
+	
+	
+	@b check
+	
 
-	/* Ingreso de datos del usuario */
-ingresoDatos:
-	
-	ldr r0,=txt_operando
-	bl puts
-	ldr r0,=getop
-	ldr r1,=op
-	bl scanf
-	
-	/* Programacion defensiva */
-	cmp r0,#0
-	bne calculos
-	bl getchar
-	ldr r0,=error
-	bl puts
-	b ingresoDatos
-	
-	
-calculos:	
-	ldr r0,=op
-	ldr r0,[r0]
-	mov r1,r5
-	
 	cmp r4, #'+' 
-	beq subroutine_suma		/* Si r5 = + suma */
-	
-	mov r1,r0
-	ldr r0,=res_sum
-	bl printf
+	beq sumar		/* Si r5 = + suma */
 	
 	cmp r4, #'-'
-	beq subroutine_resta	/* Si r5 = - resta */
+	beq Restar	/* Si r5 = - resta */
 
-	/*cmp r4, #'*'
-	beq subroutine_multip	 Si r5 = * mutiplicacion */
+	cmp r4, #'*'
+	beq multiplicacion	 /*Si r5 = * mutiplicacion */
 
 	/*cmp r4, #'M'
 	beq subroutine_div	 	Si r5 = M division */
 
-	/*cmp r4, #'P'
-	beq subroutine_pot	 Si r5 = P potencia */
+	cmp r4, #'P'
+	beq potencia	/* Si r5 = P potencia*/ 
 
 	/*cmp r4, #'='
 	beq subroutine_tot	 Si r5 = = resultado */
@@ -108,26 +85,104 @@ calculos:
 
 	@bne invalido 	@@En caso de no ser ningun valor irse a la funcion
 	
+sumar:
+	ldr r0,= txt_operando
+	bl puts
+	
+	ldr r0,= getop
+	ldr r1,=op_1
+	bl scanf
+	
+	ldr r0,=op_1
+	ldr r0,[r0]
+	mov r5,r0
+	ldr r1,=op
+	ldr r1,[r1]
+	bl subroutine_suma
+	
+	mov r1, r0
+	ldr r0,= res_sum
+	bl printf
+	ldr r10,=op
+	str r5,[r10]
+	bl getchar
+	b main
+	
+Restar:
+	ldr r0,= txt_operando
+	bl puts
+	
+	ldr r0,= getop
+	ldr r1,=op_1
+	bl scanf
+	
+	ldr r0,=op_1
+	ldr r0,[r0]
+	mov r5,r0
+	ldr r1,=op
+	ldr r1,[r1]
+	bl subroutine_resta
+	
+	mov r1, r0
+	ldr r0,= res_res
+	bl printf
+	ldr r10,=op
+	str r5,[r10]
+	bl getchar
+	b main
+	
+multiplicacion:
+	ldr r0,= txt_operando
+	bl puts
+	
+	ldr r0,= getop
+	ldr r1,=op_1
+	bl scanf
+	
+	ldr r0,=op_1
+	ldr r0,[r0]
+	mov r5,r0
+	ldr r1,=op
+	ldr r1,[r1]
+	bl subroutine_multiplicacion
+	
+	mov r1, r0
+	ldr r0,= res_mul
+	bl printf
+	ldr r10,=op
+	str r5,[r10]
+	bl getchar
+	b main
+	
+	
+potencia:
+	ldr r0,= txt_operando
+	bl puts
+	
+	ldr r0,= getop
+	ldr r1,=op_1
+	bl scanf
+	
+	ldr r0,=op_1
+	ldr r0,[r0]
+	mov r5,r0
+	ldr r1,=op
+	ldr r1,[r1]
+	bl subroutine_potencia
+	
+	mov r1, r0
+	ldr r0,= res_pot
+	bl printf
+	ldr r10,=op
+	str r5,[r10]
+	bl getchar
+	b main
+	
+	
 
 _exit:
-	mov	r3, #0	
-	mov	r0, r3
+	@mov	r3, #0	
+	@mov	r0, r3
 	ldmfd	sp!, {lr}	/* Salida correcta */
-	bx lr
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	bx	lr
 	
